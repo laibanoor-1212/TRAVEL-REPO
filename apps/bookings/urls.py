6fe26b6c6fbbc2d047ce0523573e2fd890ea76de
Tubaa Ahsan <tubaaahsan343@gmail.com>
2026-06-21 04:56:48 -0700

    user-dashboard,user-booking,user-profile,...

--- apps/bookings/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'bookings'
-urlpatterns = [
-  path('book/<int:package_id>/', views.booking, name='book_package'),
-]
--- apps/customers/apps.py
+++ apps/customers/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class CustomersConfig(AppConfig):
     name = 'customers'
+    default_auto_field = 'django.db.models.BigAutoField'
